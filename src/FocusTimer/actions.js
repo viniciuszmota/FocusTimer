228e6d00f9eacac151d29ed21c7593a5cf9c66a6
Vinicius Mota <vinicius.quimica@gmail.com>
2024-04-01 11:06:41 -0300

    sounds

--- src/FocusTimer/actions.js
+++ src/FocusTimer/actions.js
@@ -1,5 +1,25 @@
 import state from "./state.js"
+import * as timer from "./timer.js"
+import * as el from "./elements.js"
 
 export function toggleRunning() {
   state.isRunnig = document.documentElement.classList.toggle("running")
+
+  timer.countdown()
+}
+
+export function reset() {
+  state.isRunnig = false
+  state.isRunnig = document.documentElement.classList.remove("running")
+
+  timer.updateDisplay()
+}
+
+export function set() {
+  el.minutes.setAttribute("contenteditable", true)
+  el.minutes.focus()
+}
+
+export function toggleMusic() {
+  state.isRunnig = document.documentElement.classList.toggle("music-on")
 }
---
+++ src/FocusTimer/sounds.js
@@ -0,0 +1,5 @@
+export const buttonPressAudio = new Audio("./assets/button-press.wav")
+
+export const kitchenTimer = new Audio("./assets/kichen-timer.mp3")
+
+export const bgAUdio = new Audio("./assets/bg-audio.mp3")
